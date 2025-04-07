29f25f09c3f6290d37db23b7d090002a5b283964
승민 이 <rlaxodud5877@gmail.com>
2025-04-07 03:02:29 +0000

    0407

---
+++ test3.py
@@ -0,0 +1,43 @@
+def print_cat():
+    cat = [
+        "^-^"
+        ]
+    
+    for line in cat:
+        print(line) 
+for i in range(5) :
+    # print(i)
+    print_cat()
+
+def print_dog():
+    dog = [
+        "ㅡ.ㅡ"
+        ]
+    
+    for line in dog:
+        print(line) 
+for i in range(5) :
+    # print(i)
+    print_dog()
+
+def print_bird():
+    bird = [
+        "<^3^>"
+        ]
+    
+    for line in bird:
+        print(line) 
+for i in range(5) :
+    # print(i)
+    print_bird()
+
+def print_snake():
+    snake = [
+        "~~~"
+        ]
+    
+    for line in snake:
+        print(line) 
+for i in range(5) :
+    # print(i)
+    print_snake()
